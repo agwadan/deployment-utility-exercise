771faea5b609297ba16c34a4841bf0f4da42471e
agwadan <agwa.dan@gmail.com>
2021-07-24 23:54:19 +0300

    Receive request from frontend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,13 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
+const router = express.Router();
 const PORT = 4000;
 
 app.use(require('cors'));
+
+router.post('/', (req, res) => {
+  res.send(req.body);
+  console.log(req.body);
+})
+
 app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
