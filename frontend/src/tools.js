0fbf398b22113cb7fc14f41124f320b1791f211b
agwadan <agwa.dan@gmail.com>
2021-07-26 20:23:49 +0300

    Update regular expression

--- frontend/src/tools.js
+++ frontend/src/tools.js
@@ -1,5 +1,5 @@
 const isValidUrl = (url) => {
-  const urlRegEx = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+  const urlRegEx = /[-a-zA-Z0-9@:%._\+~#=]{1,256}[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
   return urlRegEx.test(url);
 }
 
