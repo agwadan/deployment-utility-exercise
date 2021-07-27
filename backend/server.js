4466c5caca7a33d180678458deafa6e7602ea96a
agwadan <agwa.dan@gmail.com>
2021-07-27 10:30:12 +0300

    Validate url in the backend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const { isValidUrl } = require('./tools');
 const app = express();
-const router = express.Router();
 const PORT = 4000;
 
 app.use(require('cors')());
 app.use(express.json());
 
 app.post('/', (req, res) => {
+  if (!isValidUrl(req.body.content)) {
+    console.log('error');
+    return res.status(403).send('Please provide a valid url');
+  }
   res.send(req.body);
-  console.log(req.body.content);
+  console.log(req.body);
 })
 
 app.get('/', (req, res) => res.send('Deployment Utility Exercise'));
