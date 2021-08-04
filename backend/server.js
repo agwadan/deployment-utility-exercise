44d0d3937400fa5cc037e141d2eeefcfe91d57fc
agwadan <agwa.dan@gmail.com>
2021-08-04 15:40:38 +0300

    Added terminal commands to deploy the app image and run the service

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,44 @@
 const express = require('express');
 const { exec } = require("child_process");
 const { isValidUrl } = require('./tools');
+const { stderr } = require('process');
 const app = express();
 const PORT = 4000;
+
+let id = 3;
 
 app.use(require('cors')());
 app.use(express.json());
 
 app.post('/', (req, res) => {
-  if (!isValidUrl(req.body.content)) {
+
+  const url = req.body.content;
+
+  if (!isValidUrl(url)) {
     console.log('Invalid URL');
     return res.status(403).send('Please provide a valid URL');
   }
 
-  exec(`docker run -p 3002:80 ${req.body.content}`, (error, stdout, stderr) => {
-    if (error) {
-      console.log(`error: ${error.message}`);
-      return;
-    }
-    console.log(`\n${stdout}`);
+  exec(`kubectl create deployment dep${id} --image=${url}`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      console.log(`\n${stdout}`);
+    })
+
+  exec(`kubectl expose deployment dep${id} --type=LoadBalancer --port=8080`,
+    (error, stdout, stderr) => {
+      console.log(`\n\n${stdout}`);
+    })
+
+  exec(`minikube service dep${id}`, (error, stdout, stderr) => {
+    console.log(`\n\n${stdout}`);
   })
 
+
+  id++;
   res.status(200).send(req.body);
   console.log(req.body);
 })
