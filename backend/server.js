b493a1c21326bd38dc04258da18e66133c73dfe9
agwadan <agwa.dan@gmail.com>
2021-08-04 16:09:38 +0300

    add random name generator for every new deployment

--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,47 @@
 const express = require('express');
-const { exec } = require("child_process");
+const { exec } = require('child_process');
 const { isValidUrl } = require('./tools');
-const { stderr } = require('process');
 const app = express();
 const PORT = 4000;
-
-let id = 3;
 
 app.use(require('cors')());
 app.use(express.json());
 
+const genName = (length) => {
+  var result = '';
+  var characters = 'abcdefghijklmnopqrstuvwxyz';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() *
+      charactersLength));
+  }
+  return result;
+}
+
 app.post('/', (req, res) => {
 
   const url = req.body.content;
+  const depName = genName(10);
 
   if (!isValidUrl(url)) {
     console.log('Invalid URL');
     return res.status(403).send('Please provide a valid URL');
   }
 
-  exec(`kubectl create deployment dep${id} --image=${url}`,
+  exec(`kubectl create deployment ${depName} --image=${url}`,
     (error, stdout, stderr) => {
-      if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-      }
       console.log(`\n${stdout}`);
     })
 
-  exec(`kubectl expose deployment dep${id} --type=LoadBalancer --port=8080`,
+  exec(`kubectl expose deployment ${depName} --type=LoadBalancer --port=8080`,
     (error, stdout, stderr) => {
       console.log(`\n\n${stdout}`);
     })
 
-  exec(`minikube service dep${id}`, (error, stdout, stderr) => {
+  exec(`minikube service ${depName}`, (error, stdout, stderr) => {
     console.log(`\n\n${stdout}`);
   })
 
-
-  id++;
   res.status(200).send(req.body);
   console.log(req.body);
 })
